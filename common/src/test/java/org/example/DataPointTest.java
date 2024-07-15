55a38ab6944bdab70fb811e3471d794b20897922
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2024-07-15 16:12:56 +0200

    Imports on DataPointTest

--- common/src/test/java/org/example/DataPointTest.java
+++ common/src/test/java/org/example/DataPointTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.datapoint.DataPoint;
+import org.example.datapoint.GenericDataPoint;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
